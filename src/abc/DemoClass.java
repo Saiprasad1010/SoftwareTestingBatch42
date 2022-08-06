519477f78c7bdae4217874f0aa4e99ea02fe923a
Saiprasad <saiprasadpatil1010@gmail.com>
2022-08-06 10:36:27 +0530

    first commit

---
+++ src/abc/DemoClass.java
@@ -0,0 +1,55 @@
+package abc;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//byte a=100;
+//System.out.println(a);
+//
+//int b=25;
+//int c=95;
+//int d=b+c;
+//System.out.println(d);
+//		
+//short e=15230;
+//System.out.println(e);
+//
+//  float f=13.25f;
+//  System.out.println(f);
+//  
+//  double g=13.2569;
+//  System.out.println(g);
+//  
+//  long h=1230658;
+//  System.out.println(h);
+//  
+//  boolean i= true;
+//  System.out.println(i);
+//  
+//  char k='z';
+//  System.out.println(k);
+//  
+//  int m=36;
+//  int n=85;
+//  int o=m*n;
+//  int p=n/m;
+//  int q=n-m;
+//  int r=m%n;
+//  
+//  System.out.println("Multiplication of 36 & 85 is (36*85):"+o);
+//  System.out.println("Division of 85 & 36 is (85/36):"+p);
+//  System.out.println("Sustraction of 85 & 36 is (85-36):"+q);
+//  System.out.println("Percentage of 36 with 85 is (36%85):"+r);
+int a=5/2;
+System.out.println(a);;
+
+  
+		
+  
+  
+  
+	}
+
+}
